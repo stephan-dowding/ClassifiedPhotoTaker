d8bf5f6dd1dd45ee190cea3f8103f8bb6ba87e93
Stephan Dowding <sdowding@thoughtworks.com>
2019-01-16 20:30:02 +0800

    Initial Commit

---
+++ ClassifiedPhotoTaker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ClassifiedPhotoTaker
+//
+//  Created by Stephan Dowding on 16/01/2019.
+//  Copyright © 2019 Stephan Dowding. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ClassifiedPhotoTaker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ClassifiedPhotoTaker
+//
+//  Created by Stephan Dowding on 16/01/2019.
+//  Copyright © 2019 Stephan Dowding. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
